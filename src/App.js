6e81abb489fdd02bb5653e9a3e9fa8de9ed83383
Daniel Thornton <D.r.thornton@hotmail.com>
2022-04-08 16:30:20 +1000

    small changes to char sheet api, and added some content to display for now

--- src/App.js
+++ src/App.js
@@ -9,21 +9,37 @@ function App() {
 
   return (
     <div data-testid="app" className="container">
+      <SpellsheetTest CharacterSheetData={CharacterSheetData} />
+      <CharacterStatsTest CharacterSheetData={CharacterSheetData} />
+    </div>
+  );
+}
+
+//Note these are here to render sample content to ensure the project is not blank during early dev phase
+
+function CharacterStatsTest({ CharacterSheetData }) {
+  return (
+    <GreyBackGroundBorder>
+      {Object.keys(CharacterSheetData.Stats).map((key) => (
+        <CharacterStats
+          key={key}
+          title={key}
+          value={CharacterSheetData.Stats[key].value}
+          modifier={CharacterSheetData.Stats[key].modifier}
+          skills={CharacterSheetData.Stats[key].skills}
+        ></CharacterStats>
+      ))}
+    </GreyBackGroundBorder>
+  );
+}
+
+function SpellsheetTest({ CharacterSheetData }) {
+  return (
+    <>
       {CharacterSheetData.spells.map((spellset) => (
         <Spells spells={spellset} key={spellset.name}></Spells>
       ))}
-      <GreyBackGroundBorder>
-        {Object.keys(CharacterSheetData.Stats).map((key) => (
-          <CharacterStats
-            key={key}
-            title={key}
-            value={CharacterSheetData.Stats[key].value}
-            modifier={CharacterSheetData.Stats[key].modifier}
-            skills={CharacterSheetData.Stats[key].skills}
-          ></CharacterStats>
-        ))}
-      </GreyBackGroundBorder>
-    </div>
+    </>
   );
 }
 
